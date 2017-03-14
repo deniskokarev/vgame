9716bc85eddc1b068d86cbd354272ddb55202078
Denis Kokarev <d_kokarev@mail.ru>
2017-03-14 18:40:38 -0400

    flash display every second

--- soft/run.cpp
+++ soft/run.cpp
@@ -14,8 +14,11 @@ Adafruit_PCD8544_HAL display = Adafruit_PCD8544_HAL(hspi1, dc, cs, rst);
 // our main event loop to be invoked from HAL main.c
 extern "C" void run() {
 	display.begin();
+	int color = BLACK;
 	while (true) {
+		display.fillScreen(color);
 		display.display();
 		HAL_Delay(1000);
+		color ^= 1;
 	}
 }
