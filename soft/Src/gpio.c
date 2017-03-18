91f46807da1fe7378eac2b21be4c11ca009e3b93
Denis Kokarev <d_kokarev@mail.ru>
2017-03-18 12:20:40 -0400

    configuring out button pins as pull-down

--- soft/Src/gpio.c
+++ soft/Src/gpio.c
@@ -83,7 +83,7 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6 
                           |GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PB0 PB1 */
